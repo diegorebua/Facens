a9f9371ab27be9f049447ed6b87d3a8e8035ed30
Diego Rebuá <diegorebua@gmail.com>
2025-09-01 21:15:00 -0300

    7

--- GTI/3_Semestre/src/Estatística/Exercícios_1/EquaçãoQuadrática.py
+++
@@ -1,5 +0,0 @@
-a = int(input("Digite o valor de a: "))
-b = int(input("Digite o valor de b: "))
-c = int(input("Digite o valor de c: "))
-
-print(f"A equação é: {a}x² + {b}x + {c} = 0")
--- GTI/3_Semestre/src/Estatística/Exercícios_1/NúmeroCrescente.py
+++
@@ -1,7 +0,0 @@
-a = int(input("Digite o primeiro número: "))
-b = int(input("Digite o segundo número: "))
-c = int(input("Digite o terceiro número: "))
-
-numeros = [a, b, c]
-numeros.sort()
-print("Os números em ordem crescente são:", numeros)
